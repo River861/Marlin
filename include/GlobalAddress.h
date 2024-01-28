a146067bab3432a139ba9a62678a3e770971537d
Xuchuan Luo <river861@qq.com>
2024-01-28 22:09:10 +0800

    debug

--- include/GlobalAddress.h
+++ include/GlobalAddress.h
@@ -1,43 +1,59 @@
-#ifndef __GLOBALADDRESS_H__
-#define __GLOBALADDRESS_H__
+#ifndef __GLOBAL_ADDRESS_H__
+#define __GLOBAL_ADDRESS_H__
 
 #include "Common.h"
 
+#include <iostream>
 
+/* Global Address */
 class GlobalAddress {
 public:
-
-union {
+  union {
   struct {
-  uint64_t nodeID: 16;
+  uint64_t nodeID : 16;
   uint64_t offset : 48;
   };
   uint64_t val;
-};
+  };
 
   GlobalAddress() : val(0) {}
   GlobalAddress(uint64_t nodeID, uint64_t offset) : nodeID(nodeID), offset(offset) {}
   GlobalAddress(uint64_t val) : val(val) {}
   GlobalAddress(const GlobalAddress& gaddr) : val(gaddr.val) {}
 
+  // operator uint64_t() const { return val; }
   uint64_t to_uint64() const {return val; }
 
   static GlobalAddress Null() {
-    static GlobalAddress zero{0, 0};
-    return zero;
+    return GlobalAddress();
+  };
+
+  static GlobalAddress Max() {
+    return GlobalAddress(MEMORY_NODE_NUM, 0);
+  };
+
+  GlobalAddress operator+(int b) const {
+    return GlobalAddress(nodeID, offset + b);
+  }
+
+  GlobalAddress operator-(int b) const {
+    return GlobalAddress(nodeID, offset - b);
+  }
+
+  static GlobalAddress Widest() {
+    return GlobalAddress(~0ULL);
   };
 } __attribute__((packed));
 
-static_assert(sizeof(GlobalAddress) == sizeof(uint64_t), "XXX");
+static_assert(sizeof(GlobalAddress) == sizeof(uint64_t));
 
-inline GlobalAddress GADD(const GlobalAddress &addr, int off) {
-  auto ret = addr;
-  ret.offset += off;
-  return ret;
-}
 
 inline bool operator==(const GlobalAddress &lhs, const GlobalAddress &rhs) {
   return (lhs.nodeID == rhs.nodeID) && (lhs.offset == rhs.offset);
+}
+
+inline bool operator<(const GlobalAddress &lhs, const GlobalAddress &rhs) {
+  return std::make_pair(lhs.nodeID, lhs.offset) < std::make_pair(rhs.nodeID, rhs.offset);
 }
 
 inline bool operator!=(const GlobalAddress &lhs, const GlobalAddress &rhs) {
@@ -49,4 +65,5 @@ inline std::ostream &operator<<(std::ostream &os, const GlobalAddress &obj) {
   return os;
 }
 
-#endif /* __GLOBALADDRESS_H__ */
+
+#endif /* __GLOBAL_ADDRESS_H__ */
