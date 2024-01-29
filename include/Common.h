b4840f800cd7722c7dc798821bd551e068c5e5d8
river861 <river861@qq.com>
2024-01-29 23:43:02 +0800

    debug

--- include/Common.h
+++ include/Common.h
@@ -167,7 +167,7 @@ constexpr Value kValueNull = 0;
 constexpr Value kValueMin = 1;
 constexpr Value kValueMax = std::numeric_limits<Value>::max();
 // fixed
-constexpr int spanSize = 32;
+constexpr int spanSize = 64;
 
 
 // calculate kInternalPageSize and kLeafPageSize
